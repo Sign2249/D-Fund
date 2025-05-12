dccbeb7804db99068aa37a9e27265b10096e422d
Sign2249 <ktw2249@naver.com>
2025-05-12 19:02:06 +0900

    git prac end

--- src/components/AllProjects.js
+++ src/components/AllProjects.js
@@ -1,6 +1,6 @@
 // AllProjects.js
 // git practice
-// git prac2
+// git prac21
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ethers } from 'ethers';
